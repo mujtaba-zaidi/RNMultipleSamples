a3ab8b56e71ef1dff447c54510dd0e43994bf5a8
Mujtaba Raza <mujtaba@inflight-vr.com>
2018-10-31 15:04:11 +0500

    Fixed: removed imports

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { StyleSheet, View } from "react-native";
-import Switch from "./Components/Switch";
 
 export default class App extends Component {
   render() {
