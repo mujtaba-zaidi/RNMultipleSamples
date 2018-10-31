1b46893cedf26f0b2e24f0f8770c72d5244f58e2
Mujtaba Raza <mujtaba@inflight-vr.com>
2018-10-31 15:00:18 +0500

    Removed: removed switch and unnecessary things to make default project.

--- App.js
+++ App.js
@@ -4,52 +4,9 @@ import Switch from "./Components/Switch";
 
 export default class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Switch
-          leftSideText={"Turn Off"}
-          rightSideText={"Turn On"}
-          activatedLeftBackgroundColor={"#f0f8ff"}
-          activatedLeftTextColor={"#5f9ea0"}
-          activatedRightBackgroundColor={"#00ffff"}
-          activatedRightTextColor={"#000000"}
-          deactivatedLeftBackgroundColor={"#00ffff"}
-          deactivatedLeftTextColor={"#000000"}
-          deactivatedRightBackgroundColor={"#f0f8ff"}
-          deactivatedRightTextColor={"#5f9ea0"}
-          containerWidth={"30%"}
-          containerHeight={"5%"}
-        />
-        <Switch
-          leftSideText={"Off"}
-          rightSideText={"On"}
-          activatedLeftBackgroundColor={"#fff8dc"}
-          activatedLeftTextColor={"#d2691e"}
-          activatedRightBackgroundColor={"#7fff00"}
-          activatedRightTextColor={"#00ffff"}
-          deactivatedLeftBackgroundColor={"#7fff00"}
-          deactivatedLeftTextColor={"#00ffff"}
-          deactivatedRightBackgroundColor={"#fff8dc"}
-          deactivatedRightTextColor={"#d2691e"}
-          containerWidth={"60%"}
-          containerHeight={"10%"}
-        />
-      </View>
-    );
+    return <View style={styles.container} />;
   }
 }
-// leftSideText,
-//       rightSideText,
-//       activatedLeftBackgroundColor,
-//       activatedLeftTextColor,
-//       activatedRightBackgroundColor,
-//       activatedRightTextColor,
-//       deactivatedLeftBackgroundColor,
-//       deactivatedLeftTextColor,
-//       deactivatedRightBackgroundColor,
-//       deactivatedRightTextColor,
-//       containerWidth,
-//       containerHeight
 const styles = StyleSheet.create({
   container: {
     flex: 1,
